eebf0524897f8ab93c480545c5d6a312db5a0df7
Joseph Nma <josgolf@yahoo.ca>
2025-09-25 20:10:24 +0100

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
 import handler from "./api/email/send.js";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 // Route maps to same handler used by Vercel
 app.post("/api/email/send", handler);
+
+app.get("/", (req, res) => {
+  res.send("✅ Email service is running. Use POST /api/email/send");
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
